4f5473bb59e84ccf3412233f164e95beebaf0642
Archy17 <1070711@gmail.com>
2017-05-26 17:47:53 +0300

    чуть чуть заработал ГС

--- gs.erl
+++ gs.erl
@@ -1,43 +1,49 @@
--module(gsl).
- 
--behaviour(gen_server).
- 
--export([start_link/0,
-        create_acc/2,
-       
- 
--export([init/1, handle_call/3, handle_info/2,
-         terminate/2, code_change/3]),
- 
+-module(gs).
 
- 
- 
+-behaviour(gen_server).
+
+
+-export([start_link/0, create/1]).
+
+
+-export([init/1, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {}).
+-define(SERVER, ?MODULE).
+
+
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
- 
-creat_acc(Name, Email) -> gen_server:call(?SERVER, {creat, Name, Email}).
- 
+
+  create(Name) ->
+  gen_server:cast(?SERVER, {create, Name}).
 
 
 init([]) ->
- ?MODULE = dets:open_file(?MODULE, [set, my_dets]).
- 
-handle_call({creat, Name, Email}, _From, State) ->
-  case dets:lookup(my_dets, Name) of
-     [{Name, _, _}] -> 
-        error,
-     [] ->
-        dets:insert(Name, Email),
-        {reply, Response , State};
+  {ok, dets:open_file(md, [])}.
+
+
+
+
+
+handle_cast({create, Name}, State) ->
+  case dets:lookup(md, Name) of
+  [{Name}] -> error,
+        
+    {noreply, State};
+  [] -> dets:insert(md, {Name}),
+  {noreply, State}
   end.
- 
- 
+
+
 handle_info(_Info, State) ->
- {noreply, State}.
- 
- 
+  {noreply, State}.
+
+
 terminate(_Reason, _State) ->
   ok.
- 
+
+
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
