03581d4df352f29b19e6273d57d02eea75180127
Arc <1070711@gmail.com>
2017-07-29 11:06:46 +0700

    более 1 функции

--- gs2.erl
+++ gs2.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 
--export([start_link/0, create/1]).
+-export([start_link/0, create/1, del/1]).
 
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -19,9 +19,11 @@ start_link() ->
   create(Name) ->
   gen_server:call(?SERVER, {create, Name}).
 
+ del(Name) ->
+  gen_server:call(?SERVER, {del, Name}).
 
 init([]) ->
-  dets:open_file(md, []).
+  {ok, dets:open_file(md, [])}.
 
 handle_call({create, Name}, _From, State) ->
  case dets:lookup(md, Name) of
@@ -29,8 +31,18 @@ handle_call({create, Name}, _From, State) ->
   {reply, {oke}, State};
   _else -> 
   {reply, {error}, State}
+  end;
+
+  
+  
+handle_call({del, Name}, _From, State) ->
+ dets:delete(md, {Name}),
+  {reply, {okedel}, State};
+  _else -> 
+  {reply, {error}, State}
   
  end.
+
 
 handle_cast(_Msg, State) ->
   {noreply, State}.
