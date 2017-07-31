4b6305dbd3877080e09a71e115afc85b3e54ee95
Archy17 <1070711@gmail.com>
2017-08-01 02:59:19 +0300

    dobavil handle call 2

--- gs.erl
+++ gs.erl
@@ -3,38 +3,46 @@
 -behaviour(gen_server).
 
 
--export([start_link/0, create/1]).
+-export([start_link/0, create/3, close/0]).
 
 
--export([init/1, handle_cast/2, handle_info/2,
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {}).
+
 -define(SERVER, ?MODULE).
 
 
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-  create(Name) ->
-  gen_server:cast(?SERVER, {create, Name}).
+  create(Name, Adress,Bissnes) ->
+  gen_server:call(?SERVER, {create, Name , Adress, Bissnes}).
 
-
+  close() -> gen_server:call(?SERVER, {close}).
+	  
 init([]) ->
-  {ok, dets:open_file(md, [])}.
+  {ok, dets:open_file(md,  [{type, set}])}.
 
+handle_call({create, Name, Adress,Bissnes}, _From, State) ->
+ case dets:lookup(md, Name) of
+  [] -> dets:insert(md, {Name, Adress,Bissnes}),
+  {reply, {oke}, State};
+  _else -> 
+  {reply, {error}, State}
+			end;
 
+handle_call({close}, _From, State) -> dets:close(md),
+	{reply, {oke}, State}
+									   end;
 
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
 
-
-handle_cast({create, Name}, State) ->
-  case dets:lookup(md, Name) of
-  [{Name}] -> error,
-        
-    {noreply, State};
-  [] -> dets:insert(md, {Name}),
-  {noreply, State}
-  end.
+									   
+handle_cast(_Msg, State) ->
+  {noreply, State}.
 
 
 handle_info(_Info, State) ->
@@ -46,4 +54,4 @@ terminate(_Reason, _State) ->
 
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+{ok, State}.
