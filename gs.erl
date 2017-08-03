b58f6a56bbb87c192f93b5d63b771715d29fb99e
Arc <1070711@gmail.com>
2017-08-03 21:23:46 +0700

    Добавил READ

--- gs.erl
+++ gs.erl
@@ -3,35 +3,64 @@
 -behaviour(gen_server).
 
 
--export([start_link/0, create/1]).
+-export([start_link/0, create/3, read/1, close/0]).
 
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {}).
+
 -define(SERVER, ?MODULE).
 
 
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-  create(Name) ->
-  gen_server:call(?SERVER, {create, Name}).
+  create(Name, Adress,Bissnes) ->
+  gen_server:call(?SERVER, {create, Name , Adress, Bissnes}).
+  
+  read(Name) ->
+  gen_server:call(?SERVER, {read, Name}).
 
-
+  close() -> gen_server:call(?SERVER, {close}).
+	  
 init([]) ->
-  dets:open_file(md, []).
+  {ok, dets:open_file(md,  [{type, set}])}.
 
-handle_call({create, Name}, _From, State) ->
+handle_call({create, Name, Adress,Bissnes}, _From, State) ->
  case dets:lookup(md, Name) of
-  [] -> dets:insert(md, {Name}),
+  [] -> dets:insert(md, {Name, Adress,Bissnes}),
   {reply, {oke}, State};
   _else -> 
   {reply, {error}, State}
-  
- end.
+			end;
 
+			
+			
+handle_call({read, Name}, _From, State) ->
+ case dets:lookup(md, Name) of
+  [] -> dets:lookup(md, Name),
+  {reply, {error}, State};
+  _else -> 
+  {reply, dets:lookup(md, Name), State}
+			end;
+			
+			
+%% handle_call({read, Name}, _From, State) ->
+%%   dets:lookup(md, Name),
+%%   {reply, {ok}, State};
+  
+			
+			
+handle_call({close}, _From, State) -> dets:close(md),
+	{reply, {okeKlose}, State};
+									 
+
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+									   
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
@@ -45,4 +74,4 @@ terminate(_Reason, _State) ->
 
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+{ok, State}.
